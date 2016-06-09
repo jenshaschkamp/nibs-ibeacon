d9caeebc73cd4696b63151e403e387c0d3c901ce
jenshaschkamp <jens.haschkamp@gmail.com>
2016-06-09 15:06:21 +0200

    Update stores.js

--- server/stores.js
+++ server/stores.js
@@ -9,7 +9,7 @@ var db = require('./pghelper'),
  */
 function findAll(req, res, next) {
 	console.log('findAll');
-    db.query("SELECT id, name, location__latitude__s AS latitude, location__longitude__s AS longitude FROM salesforce.store__c ORDER BY name")
+    db.query("SELECT id, name, location__latitude__s AS latitude, location__longitude__s AS longitude FROM salesforce1.store__c ORDER BY name")
         .then(function (stores) {
             return res.send(JSON.stringify(stores));
         })
